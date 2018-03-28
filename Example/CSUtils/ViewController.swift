478726504c0e46dac0b92cd0f23215f13c9baebf
Chrystian <salllgado@hotmail.com.br>
2018-03-28 11:37:58 -0300

    Initial commit, configure podspac, and some initial configs

--- Example/CSUtils/ViewController.swift
+++ Example/CSUtils/ViewController.swift
@@ -7,18 +7,20 @@
 //
 
 import UIKit
+import CSUtils
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showSimpleAlert(_ sender: Any) {
     }
-
+    
+    @IBAction func showCustomWebView(_ sender: Any) {
+        
+    }
 }
 
