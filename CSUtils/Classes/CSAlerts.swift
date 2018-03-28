8bc82c4d11b031455ce473ec16ad1f6e9f1ce541
Chrystian <salllgado@hotmail.com.br>
2018-03-28 17:57:13 -0300

    Introduzindo configuração de alertViewCustom (problemas para acessar o metodo de dentro do projeto exemplo)

---
+++ CSUtils/Classes/CSAlerts.swift
@@ -0,0 +1,15 @@
+//
+//  CSAlerts.swift
+//  Pods-CSUtils_Example
+//
+//  Created by Chrystian Salgado on 28/03/2018.
+//
+
+import Foundation
+
+public class CSAlerts {
+
+    public func showAlertController(){
+        print("show some alert")
+    }
+}
--- Example/CSUtils/ViewController.swift
+++ Example/CSUtils/ViewController.swift
@@ -2,21 +2,21 @@
 //  ViewController.swift
 //  CSUtils
 //
-//  Created by salllgado@hotmail.com.br on 03/28/2018.
-//  Copyright (c) 2018 salllgado@hotmail.com.br. All rights reserved.
+//  Created by Chrystian Salgado on 03/28/2018.
+//  Copyright (c) 2018 Chrystian Salgado. All rights reserved.
 //
 
 import UIKit
-//import CSUtils
+import Pods_CSUtils_Example
 
 class ViewController: UIViewController {
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func showSimpleAlert(_ sender: Any) {
+        let alert = CSAlerts().showAlertController()
     }
     
     @IBAction func showCustomWebView(_ sender: Any) {
