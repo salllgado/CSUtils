3480572af099be32f5b30e7925fb6dd4a1aa97e0
Chrystian Salgado <salllgado@hotmail.com.br>
2018-05-26 11:17:35 -0300

    #comment Novo arquivo de estilos, agora podendo setar um estilo aos botões vindo de fora, podendo colocar em varios botões o mesmo estilo ao mesmo tempo... melhorias em andamento #time 47m

--- CSUtils/Classes/CSUtils.swift
+++ CSUtils/Classes/CSUtils.swift
@@ -17,25 +17,31 @@ open class CSUtils {
      - Return: NSLocalizedString
      */
     open class func showAlertController(_ alertTitle: String?, mensage: String, alertButtons: [AlertButtonEnum], _ withButtonColor: UIColor? = nil, completion: @escaping (_ :Void?)->Void?)  -> UIAlertController {
-        let alerts = CSAlerts()
-        let alertController = alerts.buildAlert(title: alertTitle, mensage: mensage, alertButtons: alertButtons, completion: completion)
+        let csAlerts = CSAlerts()
+        let alertController = csAlerts.buildAlert(title: alertTitle, mensage: mensage, alertButtons: alertButtons, completion: completion)
         return alertController
     }
     
     open class func showAlertControllerForLogin(_ title: String?, mensage: String, alertButtons: [AlertButtonEnum], _ withButtonColor: UIColor? = nil, completion: @escaping (_ :[String]?)-> Void?)  -> UIAlertController {
-        let csalerts = CSAlerts()
-        let alertController = csalerts.buildAlertWithTextField(title: title, mensage: mensage, alertButtons: alertButtons, withButtonColor, completion: completion)
+        let csAlerts = CSAlerts()
+        let alertController = csAlerts.buildAlertWithTextField(title: title, mensage: mensage, alertButtons: alertButtons, withButtonColor, completion: completion)
         return alertController
     }
     
     open class func showAlertControllerWithFields(_ title: String?, mensage: String, alertButtons: [AlertButtonEnum], _ withButtonColor: UIColor? = nil, numberOfFields: Int,completion: @escaping (_ :[String]?)-> Void?)  -> UIAlertController {
-        let csalerts = CSAlerts()
-        let alertController = csalerts.buildAlertWithTextField(title: title, mensage: mensage, alertButtons: alertButtons, numberOfFields: numberOfFields, completion: completion)
+        let csAlerts = CSAlerts()
+        let alertController = csAlerts.buildAlertWithTextField(title: title, mensage: mensage, alertButtons: alertButtons, numberOfFields: numberOfFields, completion: completion)
         return alertController
     }
     
     open class func getLocalizableString(_ LocalizableString: String) -> String {
         let localizations = CSLocalizations()
         return localizations.setLocalizableText(LocalizableString)
+    }
+}
+
+extension CSUtils {
+    open class func setButtonStyle(_ buttons: [UIButton], textColor: UIColor?, backgoundColor: UIColor?, borderColor: UIColor?, rounded: Int?) {
+        _ = CSStyles().setButtonStyle(buttons, textColor: textColor, backgoundColor: backgoundColor, borderColor: borderColor, rounded: rounded)
     }
 }
--- Example/CSUtils/ViewController.swift
+++ Example/CSUtils/ViewController.swift
@@ -11,12 +11,21 @@ import CSUtils
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var styleButtonExemple: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func showCustomWebView(_ sender: Any) {
-        
+//        CSUtils.
+    }
+    
+    
+    @IBAction func showStylesView(_ sender: Any) {
+        var arrayOfButtons: [UIButton] = []
+        arrayOfButtons.append(styleButtonExemple)
+        CSUtils.setButtonStyle(arrayOfButtons, textColor: UIColor.yellow, backgoundColor: UIColor.black, borderColor: UIColor.yellow, rounded: 10)
     }
 }
 
